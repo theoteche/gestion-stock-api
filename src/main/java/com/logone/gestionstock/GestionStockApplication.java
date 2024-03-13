03d943bc98be7382064cb9de55d0414c463b3b07
theoteche <theoteche@gmail.com>
2024-03-13 19:24:08 +0100

    first commit

---
+++ src/main/java/com/logone/gestionstock/GestionStockApplication.java
@@ -0,0 +1,13 @@
+package com.logone.gestionstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockApplication.class, args);
+	}
+
+}
